7cd22f8d60df071ce3fa0257a08f23d7118f1f8f
BoyMughni <Boymughni242@gmail.com>
2023-10-30 13:04:56 +0700

    menambahkan beberapa file

---
+++ application/models/Contoh.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Konstruktor CI_Model
+        // ...
+    }
+
+    // Tambahkan metode lain yang sesuai dengan kebutuhan Anda di sini
+}
